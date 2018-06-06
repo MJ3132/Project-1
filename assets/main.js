1fc017d9e5c6eac99ccc4c81effeee7fd424df76
yung dev <andre.torquati@gmail.com>
2018-06-06 13:48:49 -0400

    synch'd what users should see before signing in and after signing in. Rearranged html in two sep divs, corresponding to before signing in and after (page rendering).

--- assets/main.js
+++ assets/main.js
@@ -63,41 +63,28 @@ if (user) {
     var uid = user.uid;
     var providerData = user.providerData;
     $('#login-tracker').show();
-    logOutBtn.show();
-    signInBTn.hide();
-    signUpBtn.hide();
-    $("#initialstuff").hide();
-      // ...
+    
+    renderPage('aftersignin');
     } else {
     // User is signed out.
     // ...
     $('#login-tracker').hide();
-    logOutBtn.hide();
-    signInBTn.show();
-    signUpBtn.show();
-    $("#initialstuff").show();
+    renderPage('beforesignin');
 
     }});
+
 // API Query URL + Parameters + AJAX CALLS
-
 var queryURL = "https://api.predicthq.com/v1/events?limit=1";
-
 var categories = ["conferences", "expos", "concerts", "festivals"];
-
 var search = "";
-
 var label = "";
 
-
-$("#submmit-event").on("click", function () {
-    // get random category to display
+$("#submit-event").on("click", function () {
     label = $('#event-input').val().trim();
     queryURL += "&" + $.param({
-        'labels': label,
+        'labels': label
     });
-
     console.log("query URL" + queryURL);
-
     // Call Label Response AJAX
     $.ajax({
         url: queryURL,
@@ -106,35 +93,27 @@ $("#submmit-event").on("click", function () {
         headers: {
             Authorization: "Bearer 4SepDTuqqTQQgPSM68gLJpoJJoEpSB",
             Accept: "application/json"
-
         }
     }).done(function (response) {
         console.log(response);
-
+        var responseDiv = $('<div>');
+        responseDiv.html(JSON.stringify(response.results[0]));
+        $('#answerContainer').append(responseDiv);
     }).fail(function (err) {
         // throw err;
     });
 });
-
 // End Call Label Response AJAX
 
 
 // Suprise Me button aka Gives Random Results
-
 $("#supriseMe").on("click", function () {
-
     // get random category to display
-
     var ranNum = Math.floor(Math.random() * (categories.length - 1))
-
     search = categories[ranNum];
-
     queryURL += "&" + $.param({
         'category': search,
-
-
     });
-
     $.ajax({
         url: queryURL,
         method: 'GET',
@@ -142,24 +121,21 @@ $("#supriseMe").on("click", function () {
         headers: {
             Authorization: "Bearer 4SepDTuqqTQQgPSM68gLJpoJJoEpSB",
             Accept: "application/json"
-
         }
     }).done(function (response) {
         console.log(response);
-
         var responseDiv = $('<div>');
         responseDiv.html(JSON.stringify(response.results[0]));
-
         $('#answerContainer').append(responseDiv);
-
     }).fail(function (err) {
         // throw err;
     });
 });
+
 // Page Rendering Function ( shows specific page, while hiding the other containers with the class of -page)
 function isPageShownCurrently(page) {
     return false;
-}
+};
 // [class*] all classes that have or end with]
 function renderPage(page) {
     if (!$(`.${page}-page`).is(':visible')) {
@@ -167,20 +143,14 @@ function renderPage(page) {
             $(`.${page}-page`).show(400);
         });
     }
-}
+};
 // End page rendering function
-
-
 // hide all pages except ...
 
-
 //delete button
-
 $(document).on("click", "#delete", removeTask);
-
 // Function to remove a task.
 function removeTask() {
-
     // Grab the closest div to the element that was clicked and remove it.
     // (In this case, the closest element is the one that's encapsulating it.)
     $(this).closest("div").remove();
