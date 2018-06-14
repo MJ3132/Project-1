29fa24961f8f4a6a56c431ce8fb947153bc5d589
yung dev <andre.torquati@gmail.com>
2018-06-14 01:29:38 -0400

    pushing my version of html, css, and js - will try to merge with master

--- assets/main.js
+++ assets/main.js
@@ -1,25 +1,5 @@
 let userLocation;
-var geocoder;
-
-
-function reverseGeo(coordinates) {
-    // argument is a string type : lat,lng
-    var settingsGoogle = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + coordinates + "&key=AIzaSyCjU4tP9cgPggLk_lGUSzkwW75GgrsyLCY",
-        "method": "GET",
-    };
-    $.ajax(settingsGoogle).done(function (response) {
-        //extracted the info we need 
-        // var country = response.results[0].address_components[6].long_name;
-        // var stateProvince = response.results[0].address_components[5].long_name;
-        // var city = response.results[0].address_components[3].long_name;
-        console.log(response);
-    });
-
-};
-
+// runVideo();
 // 
 function getLocation() {
     if (navigator.geolocation) {
@@ -28,7 +8,6 @@ function getLocation() {
         console.log("Geolocation is not supported by this browser.");
     }
 };
-
 function showPosition(position) {
     userLocation = position.coords;
     console.log(userLocation);
@@ -46,7 +25,6 @@ function showPosition(position) {
         var country = response.results[0].address_components[6].long_name;
         var stateProvince = response.results[0].address_components[5].long_name;
         var city = response.results[0].address_components[3].long_name;
-        console.log(response);
     });
 };
 
@@ -67,10 +45,6 @@ function showError(error) {
             break;
     }
 };
-
-
-
-
 
 ///user authentication stuff 
 var config = {
@@ -147,157 +121,65 @@ firebase.auth().onAuthStateChanged(function (user) {
     }
 });
 
-//Next Button
-
-$('#next-option').on('click', function () {
-    var counter = 0;
-    var activePost = $('.post.active')
-    var nextActivePost = activePost.next();
-    activePost.removeClass('active');
-    if (nextActivePost.length > 0) {
-        nextActivePost.addClass('active');
-
-    } else {
-        activePost.siblings().first().addClass('active');
-    }
-     updateActivePostWithAddress();
-    // End Next Button   
-
-})
-
 // API Query URL + Parameters + AJAX CALLS
-var categories = ["expos", "concerts", "festivals", "performing-arts", "sports", "community", "conferences"];
-
-// Suprise Me button aka Gives Random Results
-$("#suprise-me").on("click", function () {
-    document.querySelector('#answer-div').innerHTML = '';
-    var category = categories[Math.floor(Math.random() * (categories.length + 1))]
-
-    $.ajax({
-        url: `https://api.predicthq.com/v1/events/?within=10km@${userLocation.latitude}%2C${userLocation.longitude}&category=${category}`,
-        method: 'GET',
-        contentType: "application/json",
-        headers: {
-            Authorization: "Bearer 4SepDTuqqTQQgPSM68gLJpoJJoEpSB",
-            Accept: "application/json"
-        }
-    }).done(function (response) {
-        console.log(response);
-
-        var answer = response.results;
-        console.log(answer);
-
-        var content = answer.map(function (eachEvent) {
-            console.log(eachEvent);
+var queryURL = "https://api.predicthq.com/v1/events?limit=1";
+var categories = ["conferences", "expos", "concerts", "festivals"];
+var search = "";
+var label = "";
+var queryURL = queryURL +
 
 
-            return {
-
-                title: eachEvent.title,
-                location: {
-                    lat: eachEvent.location[1],
-                    lng: eachEvent.location[0]
-                },
-                start: eachEvent.start,
-                // end: eachEvent.end,
-                duration: eachEvent.duration,
-                description: eachEvent.description
-
-            };
+    // Suprise Me button aka Gives Random Results
+    $("#suprise-me").on("click", function () {
+        // get random category to display
+        var ranNum = Math.floor(Math.random() * (categories.length - 1))
+        search = categories[ranNum];
+        queryURL += "&" + $.param({
+            'category': search,
         });
-        console.log(content);
+        $.ajax({
+            url: `https://api.predicthq.com/v1/events/?limit=1&within=10km@${userLocation.latitude}%2C${userLocation.longitude}`,
+            method: 'GET',
+            contentType: "application/json",
+            headers: {
+                Authorization: "Bearer 4SepDTuqqTQQgPSM68gLJpoJJoEpSB",
+                Accept: "application/json"
+            }
+        }).done(function (response) {
+            console.log(response);
+            console.log(userLocation.latitude);
+            console.log("hello world");
+            var answer = response.results;
+            console.log(answer);
+            var content = answer.map(function (eachEvent) {
+                console.log(eachEvent);
 
-        var html = "";
+                return {
+                    title: eachEvent.title,
+                    duration: eachEvent.duration
+                };
 
-        for (var i = 0; i < content.length; i++) {
+            });
 
-
-            var postHTML = ` 
-            
-                <div class="post${i === 0 ? " active" : ""}">
-                    <input type='hidden' value='${content[i].location.lat}' name='lat'>
-                    <input type='hidden' value='${content[i].location.lng}' name='lng'>
-                    
-                    <p>${i + 1}/${content.length}</p>
+            console.log(content);
+            var html = "";
+            for (var i = 0; i < content.length; i++) {
+                var postHTML = ` 
+                <div class="post">
                     <h1 class="post-title">${content[i].title}</h1>
-                    <p class="post-description">${content[i].description}</p>
-                    <h2 class="post-location"></h2>
-                    <h2 class="post-start">${content[i].start}</h2>
-                    <h2 class="post-end">${content[i].end}</h2>
-                    <h2 class="post-duration">${content[i].duration}</h2>
+                    <h1 class="post-title">${content[i].duration}</h1>
 
                 </div>
             `;
-            console.log(postHTML);
-            html += postHTML;
-        }
-
-        $('#answer-div').html(html);
-        updateActivePostWithAddress();
-
-
-    }).fail(function (err) {
-        // throw errs
-    });
-});
-
-
-function updateActivePostWithAddress() {
-
-    // get actively shown post aka active element,
-    var activePost = $('.post.active');
-
-    // Change date formats
-
-    var startTime = activePost.find("input[class ='post-start']").val();
-    var changedStart = moment(startTime).format('MMMM Do YYYY, h:mm a');
-    activePost.find('.post-start').text(changedStart);
-
-    // var endTime = activePost.find("input[class ='post-end']").val();
-    // var changedEnd = moment(endTime).format('MMMM Do YYYY, h:mm a');
-    // activePost.find('.post-end').text(changedEnd);
-
-
-    // change Time Duration
-
-    
-
-
-    var duration = activePost.find("input[class ='post-duration']").val();
-    var formattedDur = moment.utc(duration*1000).format('H:mm');
-    activePost.find('.post-duration').text(formattedDur);
-
-
-    console.log(startTime);   
-    // get latitude and longitude in a var
-
-    var lat = parseFloat(activePost.find("input[name ='lat']").val());
-
-    var lng = parseFloat(activePost.find("input[name ='lng']").val());
-
-
-
-    // reverse geo code latitude and longitude 
-
-    geocoder.geocode({ 'location': { lat: lat, lng: lng } }, function (results, status) {
-        console.log(status, results);
-        if (status === 'OK') {
-            if (results[0]) {
-
-                activePost.find('.post-location').text(results[0].formatted_address);
-
-            } else {
-                window.alert('No results found');
+                console.log(postHTML);
+                html += postHTML;
             }
-        } else {
-            window.alert('Geocoder failed due to: ' + status);
-        }
+            $('#answer-div').html(html);
+        }).fail(function (err) {
+            // throw errs
+        });
     });
-    // when we get reponse back (callback function), check for status and check for address, and add the address somewhere in the active post
 
-
-
-}
 
 
 // Page Rendering Function ( shows specific page, while hiding the other containers with the class of -page)
@@ -310,118 +192,48 @@ function renderPage(page) {
         $('[class*="-page"]').hide(400, function () {
             $(`.${page}-page`).show(400);
         });
-    };
+    }
 };
-
 // End page rendering function
 // hide all pages except ...
-
-delete button
-$(document).on("click", "#delete", removeTask);
-// Function to remove a task.
-function removeTask() {
-
-    // Grab the closest div to the element that was clicked and remove it.
-    // (In this case, the closest element is the one that's encapsulating it.)
-
-    $(this).closest("div").remove();
-};
-
-
-
-
-//Reverse Geo-code coordinates to address from API response(location)
-
-
-function initMap() {
-
-    geocoder = new google.maps.Geocoder;
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // once signed render suprise me page 
 // renderPage('signup');
 
+//video background
+var video = $('.background-video');
+function runVideo(){
+    video.get(0).play();
+};
 
+function pauseVideo() {
+  video.get(0).pause();  
+};
 
-// how for loop every event
+$('#play').on('click', function(e) 
+    {
+    runVideo();
+    console.log('play');
+}
+);
 
+$('#pause').on('click', function(e) 
+    {
+    pauseVideo();
+    console.log('pause');
+}
+);
 
+//doing the footer stuff rn
 
+$('footer').on('click',function(e) {
+    console.log('waddup');
+    document.getElementById('overlay').style.display = "block";
+});
 
+$('#overlay').on('click',function(e) {
+    console.log('waddup');
+    document.getElementById('overlay').style.display = "none";
+});
 
-// Query.ajax("https://www.reddit.com/.json").done(function(response) {
-//     // console.log(response);
-//     var children = response.data.children; // easier to work with
-//     var posts = [];
-//     console.log(posts);
-//     for (var i = 0; i < children.length; i++) {
-//         var myObject = {
-//             title: children[i].data.title,
-//             author: children[i].data.author
-//         };
-//         posts.push(myObject);
-//     }
-
-    // optionally, use map to transform the data to
-    // return a *NEW* array (doesn't modify the existing array)
-    // with the new element
-    // DOES THE EXACT SAME THING!!!!!
-//     var posts2 = children.map(function(eachElement) {
-//         // return what you want the new element to be
-//         return {
-//             title: eachElement.data.title,
-//             author: eachElement.data.author
-//         };
-//     });
-
-//     renderPosts(posts);
-// });
-
-// function renderPosts(posts) {
-//     // [{title: "...", author: "..."}, ...]
-//     var html = "";
-//     for (var i = 0; i < posts.length; i++) {
-//         var postHMTL = `
-//           <div class="post">
-//             <h1 class="post-title">${posts[i].title}</h1>
-//             <span class="post-author">(${posts[i].author})</span>
-//           </div>
-//         `;
-//         console.log(postHMTL);
-//         html += postHMTL;
-//     }
-//     document.body.innerHTML = html;
-// }
-
-
-   // // get random category to display
-    // // var ranNum = Math.floor(Math.random() * (categories.length -1))
-    // search = categories[ranNum];
